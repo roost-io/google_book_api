9c970e2cb754827542ea0e88396d814b1077688d
Priyanshi Somani <priyanshisomani23105@gmail.com>
2022-06-15 14:53:19 +0530

    Changes for css file

--- ui/main.go
+++ ui/main.go
@@ -21,7 +21,17 @@ func main() {
 	http.FileServer(http.FS(res))
 	http.HandleFunc("/", homeHandler)
 	fs := http.FileServer(http.Dir("templates"))
-	http.Handle("/css/", fs)
-	// http.Handle("/js/", fs)
-	http.ListenAndServe(":80", nil)
+	http.Handle("/css/", http.StripPrefix("/css", fs))
+
+	// http.Handle("/", http.FileServer(http.Dir("css/")))
+	// fs := http.FileServer(http.Dir("templates"))
+	// http.Handle("/css/", fs)
+	// http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates"))))
+	// http.Handle("/", http.FileServer(http.Dir("css/")))
+	// http.Handle("/ui/", http.StripPrefix("templates/css/", http.FileServer(http.Dir("templates/css"))))
+	// http.HandleFunc("/css/", func(response http.ResponseWriter, request *http.Request) {
+	// 	http.ServeFile(response, request, "/templates/css/")
+	// })
+
+	http.ListenAndServe(":8084", nil)
 }
